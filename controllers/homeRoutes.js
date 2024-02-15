8db420108b2da3345bc29831e4920ad77fe9b218
Tiny Penguin <chase.seeberger@gmail.com>
2024-02-14 19:29:59 -0800

    updated home controller with login route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,10 +4,23 @@ const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
   try {
+    // retrieve the user's data from the database
+    // send the user's data to the homepage template
     res.render('homepage');
   } catch (err) {
     res.status(500).json(err);
   }
+});
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  // Otherwise, send the login page
+  res.render('login');
 });
 
 module.exports = router;
