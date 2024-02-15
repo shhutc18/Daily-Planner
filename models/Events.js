0bc6d0ca6885a191b3fa1ac24e6e8edadbe2f3bb
willber10 <willberschickler@gamil.com>
2024-02-14 20:10:02 -0800

    Begun to properly create the tables the correct way through js

---
+++ models/Events.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const e = require('express');
+
+event.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        event_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        event_date: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        event_time: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+        event_location: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'event'
+    }
+);
