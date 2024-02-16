4dffed618d3d2804ba192a223bd4cf3412c35b46
willber10 <willberschickler@gamil.com>
2024-02-15 18:45:45 -0800

    deleted the un needed schema code, added to the index.js

--- models/index.js
+++ models/index.js
@@ -2,4 +2,24 @@ const User = require('./User');
 const Events = require('./Events');
 const Tasks = require('./Tasks');
 
+User.hasMany(Events, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Events.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Tasks, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Tasks.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+
+
 module.exports = { User, Events, Tasks };
