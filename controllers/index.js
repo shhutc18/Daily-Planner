c459cd81fc00a471e97a8b0aaba5b2a8fed7aacc
Shelby Hutchinson <shhutc18@gmail.com>
2024-02-17 14:51:10 -0800

    added proper routes for auth

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
-//const apiRoutes = require('./api/');
+const apiRoutes = require('./api/');
 const homeRoutes = require('./homeRoutes');
+const authRouter = require('./auth');
 
 router.use('/', homeRoutes);
-//router.use('/api', apiRoutes);
+router.use('/api', apiRoutes);
+router.use('/', authRouter);
 
 module.exports = router;
