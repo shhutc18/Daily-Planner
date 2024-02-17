55741c375a97c59cd94f65310392dde0973aa948
Tiny Penguin <chase.seeberger@gmail.com>
2024-02-17 15:14:27 -0800

    fixed some bugs in the auth file that were causing errors authenticating users

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,24 +1,22 @@
-// Passport authentication
-
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var LocalStrategy = require('passport-local');
 var crypto = require('crypto');
 const { User } = require('../models');
+const util = require('util');
+const pbkdf2 = util.promisify(crypto.pbkdf2);
 
-passport.use(new LocalStrategy(function verify(username, password, cb) {
-  const userData = User.findOne({where: {name: username} });
+passport.use(new LocalStrategy(async function verify(username, password, cb) {
   try {
+    const userData = await User.findOne({where: {name: username} });
     if (!userData) { return cb(null, false, { message: 'Incorrect username or password.' }); }
 
-    crypto.pbkdf2(password, userData.salt, 310000, 32, 'sha256', function(err, hashedPassword) {
-      if (err) { return cb(err); }
-      if (!crypto.timingSafeEqual(userData.hashed_password, hashedPassword)) {
-        return cb(null, false, { message: 'Incorrect username or password.' });
-      }
-      return cb(null, userData);
-    });
+    const hashedPassword = await pbkdf2(password, userData.salt, 310000, 32, 'sha256');
+    if (!crypto.timingSafeEqual(Buffer.from(userData.hashed_password, 'hex'), hashedPassword)) {
+      return cb(null, false, { message: 'Incorrect username or password.' });
+    }
+    return cb(null, userData);
   } catch (err) {
     return cb(err);
   }
