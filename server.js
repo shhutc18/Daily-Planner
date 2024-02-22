2c73c10207acf63662c84fc383230927e33bae77
Tiny Penguin <chase.seeberger@gmail.com>
2024-02-21 20:09:51 -0800

    added middleware for debugging

--- server.js
+++ server.js
@@ -35,6 +35,11 @@ app.use(session(sess));
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use((req, res, next) => {
+  console.log('User: ', req.user);
+  next();
+});
+
 // Registering the handlebars view engine with Express.js
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
