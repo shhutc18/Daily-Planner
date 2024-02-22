2b61dadcd332b6428357ac9505b2b43d20765575
Tiny Penguin <chase.seeberger@gmail.com>
2024-02-21 19:10:53 -0800

    adding the passportjs session to server.js

--- server.js
+++ server.js
@@ -6,11 +6,15 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 const passport = require('passport');
+const session = require('express-session');
 
 const sequelize = require('./config/connection');
 
 // Creating a new sequelize store using the express-session package
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// Creating a new SQLite store using the express-session package
+const SQLiteStore = require('connect-sqlite3')(session);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -22,12 +26,13 @@ const sess = {
   secret: 'Super secret daily planner key',
   resave: false,
   saveUninitialized: false,
+  store: new SQLiteStore({ db: 'sessions.db', dir: './var/dv' })
 };
 
 // Adding express-session and storing as Express.js middleware
 app.use(session(sess));
 app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.authenticate('session'));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
