2d6e96ab57e60813532040086aa7cd2f78eb7ddc
Tiny Penguin <chase.seeberger@gmail.com>
2024-02-21 19:02:46 -0800

    removing comments from debugging

--- public/js/login.js
+++ public/js/login.js
@@ -1,12 +1,10 @@
 const loginFormHandler = async (event) => {
     // Stop the browser from submitting the form so it can be done with JavaScript
     event.preventDefault();
-    console.log("loginFormHandler called");
 
     // Gather the data from the form elements on the page
     const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
-    console.log(email, password);
 
     if (email && password) {
         // Send the e-mail and password to the server
