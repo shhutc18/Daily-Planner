c7475dd2d4de5402b3a1367bd8bd84d51c0bc86a
Tiny Penguin <chase.seeberger@gmail.com>
2024-02-22 20:15:19 -0800

    created a post request for the event form

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const ensureAuthenticated = require('../utils/auth');
+const { Day, Event, Todo, User } = require('../models');
+const e = require('express');
 
 
 router.get('/', ensureAuthenticated, async (req, res) => {
@@ -36,13 +38,28 @@ router.get('/event', ensureAuthenticated, async (req, res) => {
 
 router.post('/event', ensureAuthenticated, async (req, res) => {
   try {
-    const userData = req.session.passport.user;
-    const eventData = {
-      title: req.body.title,
-      description: req.body.description,
-      date: req.body.date,
-      time: req.body.time
+    const userData = req.user;
+    // finding if the day exists
+    const day = Day.findOne({ where: { date: req.body.date } });
+    let dayId;
+    // if the day does not exist, create a new day
+    if (!day || day == null) {
+      const newDay = await Day.create({ date: req.body.date, user_id: userData.id });
+      dayId = newDay.id;
+    } else {
+      dayId = day.id;
     }
+    // create a new event
+    const eventData = {
+      event_name: req.body.event_name,
+      event_time: req.body.event_time,
+      event_location: req.body.event_location,
+      event_length: req.body.event_length,
+      day_id: dayId
+    }
+
+    const newEvent = await Event.create(eventData);
+    res.status(200).json(newEvent);
 
   } catch (err) {
     res.status(500).json(err);
